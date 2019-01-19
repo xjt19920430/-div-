456050c272a406a382cd9d6db91fc95b0bf83214
nxiejingtao <xiejingtao002@deppon.com>
2019-01-19 11:06:27 +0800

    优化了一些代码，添加了注释

--- particleAnimation.js
+++ particleAnimation.js
@@ -1,10 +1,19 @@
 ;(function (window) {
-    function particleAnimation(STARTINDEX, ENDINDEX, ELEMENT, objparameter) {
-        this.STARTINDEX = STARTINDEX
-        this.ENDINDEX = ENDINDEX
-        this.ELEMENT = ELEMENT
-        this.objparameter = objparameter
-
+    function particleAnimation(ENDINDEX, ELEMENT, objparameter) {
+        this.STARTINDEX = 0 // 这个固定起始是0
+        this.ENDINDEX = ENDINDEX //  这个是要创建多少个div
+        this.ELEMENT = ELEMENT //这个是要插入的父元素
+        this.objparameter = objparameter //这个是生成粒子div的样式，必填；
+        // objparameter ={
+        //     width:2, //粒子的宽
+        //     height:2, //粒子的高
+        //     bottomADNtop:'top', //粒子动画的方向
+        //     leftANDright:'left',//粒子动画的方向
+        //     animation:'_t_center_centerTo 1s infinite linear', //动画函数，动画需要自己写
+        //     leftNum:471, // 粒子随机生成的范围，根据父元素的宽来制定
+        //     topNum:40,// 粒子随机生成的范围，根据父元素的高来制定
+        //     color:'#00b4ff' //粒子生成的颜色
+        // }
     }
 
     particleAnimation.prototype.DIVInit = function (parameter) {
